8b7e7526e55e09743faacc2e48f489d285a23df2
Danny van Bruggen <hexagonaal@gmail.com>
2018-08-10 21:18:39 +0200

    Stop release plugin from running this test.

--- src/test/java/com/laamella/mpu6502/Mpu6502Test.java
+++ src/test/java/com/laamella/mpu6502/Mpu6502Test.java
@@ -10,31 +10,30 @@ import java.nio.file.Files;
 import static com.laamella.mpu6502.RunState.RUNNING;
 import static org.junit.jupiter.api.Assertions.fail;
 
-@Disabled
-public class Mpu6502Test {
-
-    /**
-     * Runs the test suite found here: https://github.com/Klaus2m5/6502_65C02_functional_tests
-     */
-    @Test
-    public void testSuite() throws IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("6502_functional_test.bin").getFile());
-        byte[] bytes = Files.readAllBytes(file.toPath());
-        OnlyMemoryBus bus = new OnlyMemoryBus(bytes);
-        Mpu6502 mpu = new Mpu6502(bus);
-        mpu.pc = 0x0400;
-
-        RunState running;
-        do {
-            final int oldPc = mpu.pc;
-            running = mpu.step();
-            if (mpu.pc == 0x3469) {
-                return;
-            }
-            if (mpu.pc == oldPc) {
-                fail(String.format("at %04x after %d cycles.", mpu.pc, mpu.cycles));
-            }
-        } while (running == RUNNING);
-    }
-}
+//public class Mpu6502Test {
+//
+//    /**
+//     * Runs the test suite found here: https://github.com/Klaus2m5/6502_65C02_functional_tests
+//     */
+//    @Test
+//    public void testSuite() throws IOException {
+//        ClassLoader classLoader = getClass().getClassLoader();
+//        File file = new File(classLoader.getResource("6502_functional_test.bin").getFile());
+//        byte[] bytes = Files.readAllBytes(file.toPath());
+//        OnlyMemoryBus bus = new OnlyMemoryBus(bytes);
+//        Mpu6502 mpu = new Mpu6502(bus);
+//        mpu.pc = 0x0400;
+//
+//        RunState running;
+//        do {
+//            final int oldPc = mpu.pc;
+//            running = mpu.step();
+//            if (mpu.pc == 0x3469) {
+//                return;
+//            }
+//            if (mpu.pc == oldPc) {
+//                fail(String.format("at %04x after %d cycles.", mpu.pc, mpu.cycles));
+//            }
+//        } while (running == RUNNING);
+//    }
+//}
